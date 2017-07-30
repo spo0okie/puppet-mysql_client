727f75205c57a84d672195f01d6e2a7b7aed8976
root <root@main.reviakin.net>
2017-07-30 15:46:11 +0500

    initial

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+class mysql_client {
+	case $::operatingsystem {
+		Debian: {
+			package {
+				'mysql-client':			ensure => installed;
+				'libmysqlclient-dev':	ensure => installed
+				
+			}
+		}
+		CentOS: {
+			package {
+				'mysql':			ensure => installed;
+				'mysql-devel':		ensure => installed
+			}
+		}
+	}
+}
